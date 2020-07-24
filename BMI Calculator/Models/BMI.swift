5af221bcdc72d736f8fcc94f8ab905a358311c50
Amey Viney <amey@Ameys-Mac.local>
2020-07-24 12:57:28 -0700

    MVC added

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Amey Viney on 7/24/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
